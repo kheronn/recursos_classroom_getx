a910373e42d20c1ea2df76522fcef75c3e82ccef
Kheronn <kheronn@gmail.com>
2020-07-29 16:53:16 -0300

    Refactor splashpage

--- lib/modules/home/splash_page.dart
+++ lib/modules/home/splash_page.dart
@@ -22,51 +22,59 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        decoration: _buildFundoGradiente(),
-        margin: EdgeInsets.only(left: 4.0, right: 4.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              SizedBox(width: 2.0),
-              Container(
-                alignment: Alignment.center,
-                width: 50,
-                child: SvgPicture.asset(
-                  "assets/images/recursos.svg",
-                  fit: BoxFit.contain,
-                  width: 250,
-                ),
-              ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 65,
-                      width: 65,
-                      child: CircularProgressIndicator(),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 24.0),
-                      child: Text(
-                        'RECURSOS GOOGLE PARA EDUCADORES E ALUNOS',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.lato(
-                            color: Colors.black87,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+      body: _buildBody(),
+    );
+  }
+
+  Container _buildBody() {
+    return Container(
+      decoration: _buildFundoGradiente(),
+      margin: EdgeInsets.only(left: 4.0, right: 4.0),
+      child: _buildImageAndText(),
+    );
+  }
+
+  Center _buildImageAndText() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(width: 2.0),
+          Container(
+            alignment: Alignment.center,
+            width: 50,
+            child: SvgPicture.asset(
+              "assets/images/recursos.svg",
+              fit: BoxFit.contain,
+              width: 250,
+            ),
           ),
-        ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 65,
+                  width: 65,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: Text(
+                    'RECURSOS GOOGLE PARA EDUCADORES E ALUNOS',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        color: Colors.black87,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
